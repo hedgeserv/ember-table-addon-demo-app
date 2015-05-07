97ccd727cee5c6f2fc7d8f7a225671235d598626
Chaoyang <cyjia@thoughtworks.com>
2015-05-07 12:11:42 +0800

    Extract mountebank-util from test.

--- tests/integration/fully-loaded-set-of-loans-test.js
+++ tests/integration/fully-loaded-set-of-loans-test.js
@@ -2,85 +2,48 @@ import Ember from "ember";
 import DS from "ember-data";
 import { module, test } from 'qunit';
 import startApp from '../helpers/start-app';
-import config from '../../config/environment';
-
+import mb from './mountebank-util';
 
 var App;
 
 module('Fully loaded set of loans', {
   beforeEach: function () {
     App = startApp();
+
+    App.prepareLoans =  function (count) {
+      var loans = [];
+      for (var i = 0; i < count; i++) {
+        loans.push({
+          "id": i,
+          "activity": "activity " + i,
+          "status": "status " + i
+        });
+      }
+      return {
+        "header": {
+          "page": 1
+        },
+        "loans": loans
+      };
+    };
+
   },
   afterEach: function () {
     Ember.run(App, App.destroy);
   }
 });
 
-//TODO: extract to util function,
-//TODO: load data from dataset files
-function setupImposter() {
-  var loans = [];
-  for(var i =0; i< 3500; i++) {
-    loans.push({
-      "id": i,
-      "activity": "activity " + i,
-      "status": "status " + i
-    });
-  }
-  var setupPromise = Ember.$.ajax({
-    type: "POST",
-    url: config.mountebankServerHost + "/imposters",
-    data: JSON.stringify({
-      "port": 8888,
-      "protocol": "http",
-      "name": "test",
-      "stubs": [
-        {
-          "responses": [
-            {
-              "is": {
-                "headers": {
-                  "Content-Type": "application/json",
-                  "Access-Control-Allow-Origin": "*"
-                },
-                "body": JSON.stringify({
-                  "header": {
-                    "page": 1
-                  },
-                  "loans": loans
-                })
-              }
-            }
-          ],
-          "predicates": [
-            {
-              "equals": {
-                "method": "GET",
-                "path": "/loans"
-              }
-            }
-          ]
-        }
-      ]
-    }),
-    dataType: "json"
-  });
-  return setupPromise;
-}
+
 test("Should show all loans in a table", function (assert) {
-  var deletePromise = Ember.$.ajax({
-    type: "DELETE",
-    url: config.mountebankServerHost + "/imposters/8888"
-  });
+  var loans = App.prepareLoans(3500);
 
-  return deletePromise.then(function() {
-    return setupImposter().then(function() {
-      assert.expect(1);
-      visit('/fully-loaded-loans');
+  return mb.stubForOne("GET", "/loans", loans).then(function () {
+    assert.expect(1);
 
-      andThen(function () {
-        assert.equal(find('.ember-table-body-container .ember-table-table-row').length, 3502, "Page contains list of models 3502");
-      });
+    visit('/fully-loaded-loans');
+
+    andThen(function () {
+      assert.equal(find('.ember-table-body-container .ember-table-table-row').length, 3502, "Page contains list of models 3502");
     });
   });
 });
---
+++ tests/integration/mountebank-util.js
@@ -0,0 +1,55 @@
+import Ember from "ember";
+import config from '../../config/environment';
+
+export default {
+  stubForOne: function (method, path, body) {
+    return this.stubFor([{
+      method: method,
+      path: path,
+      body: body
+    }]);
+  },
+
+  stubFor: function (stubs) {
+    var mbStubs = stubs.map(function (stub) {
+      return {
+        "responses": [
+          {
+            "is": {
+              "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+              },
+              "body": JSON.stringify(stub.body)
+            }
+          }
+        ],
+        "predicates": [
+          {
+            "equals": {
+              "method": stub.method,
+              "path": stub.path
+            }
+          }
+        ]
+      };
+    });
+
+    return Ember.$.ajax({
+      type: "DELETE",
+      url: config.mountebankServerHost + "/imposters/8888"
+    }).then(function () {
+      return Ember.$.ajax({
+        type: "POST",
+        url: config.mountebankServerHost + "/imposters",
+        data: JSON.stringify({
+          "port": 8888,
+          "protocol": "http",
+          "name": "test",
+          "stubs": mbStubs
+        }),
+        dataType: "json"
+      });
+    });
+  }
+};
