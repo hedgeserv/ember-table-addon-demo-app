7ec807cb7bd08ce9302e21f74596d7a8a01fc7b9
yscui <twer@CNtwer-42.local>
2015-05-08 14:16:37 +0800

    Add install-dependencies.sh script for auto setup environment

---
+++ install-dependencies.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "${green}[Setup Script]Install Bower ...${reset}"
+npm install -g bower
+
+echo "${green}[Setup Script]Install Ember-cli ...${reset}"
+npm install -g ember-cli
+
+echo "${green}[Setup Script]Install phantomjs ...${reset}"
+npm install -g phantomjs
+
+echo "${green}[Setup Script]Install python ...${reset}"
+brew install python
+
+echo "${green}[Setup Script]Install mountebank --production ...${reset}"
+npm install -g mountebank --production
+
+echo "${green}[Setup Script]Ready to link ember-table ...${reset}"
+cd /Users/hedgebeijing/.jenkins/workspace/ember-table-addon
+npm link
+
+echo "${green}[Setup Script]Complete link to ember-table ...${reset}"
+cd /Users/hedgebeijing/.jenkins/workspace/ember-table-addon-demo-app
+npm link ember-table
+
+echo "${green}[Setup Script]Install server-side modules locally ...${reset}"
+npm install
+
+echo "${green}[Setup Script]Install client-side modules locally ...${reset}"
+bower install
+
+echo "${green}[Setup Script]Install pyton requirements ...${reset}"
+pip install -r python-webdriver-tests/requirements.txt
+
+echo "${green}[Setup Script]Install successfully ...${reset}"
