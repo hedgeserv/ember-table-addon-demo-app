d20814af9c37e554e14443dbc540eadedc4c9806
li-qiang <kata.Qlee@gmail.com>
2015-05-12 17:18:00 +0800

    set chrome options in lettuce

--- python-webdriver-tests/features/terrain.py
+++ python-webdriver-tests/features/terrain.py
@@ -1,9 +1,13 @@
 from lettuce import before, after, world
 from selenium import webdriver
 
+
 @before.all
 def setup_browser():
-    world.browser = webdriver.Firefox()
+    driver_arguments = {'chrome_options': webdriver.ChromeOptions()}
+    driver_arguments['chrome_options'].add_argument('--no-sandbox')
+    world.browser = webdriver.Chrome(chrome_options=driver_arguments['chrome_options'])
+
 
 @after.all
 def teardown_browser(total):
