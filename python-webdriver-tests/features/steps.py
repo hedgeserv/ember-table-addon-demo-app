28479c19b7014aa085c4ce63e18c4ad94f440e5f
li-qiang <kata.Qlee@gmail.com>
2015-05-13 13:20:59 +0800

    restart mountebank when run lettuces

--- python-webdriver-tests/features/steps.py
+++ python-webdriver-tests/features/steps.py
@@ -5,6 +5,7 @@ import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0,parent_dir)
+
 from prepare_loans import prepare_loans
 
 
--- python-webdriver-tests/features/terrain.py
+++ python-webdriver-tests/features/terrain.py
@@ -1,9 +1,10 @@
 from lettuce import before, after, world
 from selenium import webdriver
-
+import os
 
 @before.all
 def setup_browser():
+    os.system('mb restart &')
     driver_arguments = {'chrome_options': webdriver.ChromeOptions()}
     driver_arguments['chrome_options'].add_argument('--no-sandbox')
     world.browser = webdriver.Chrome(chrome_options=driver_arguments['chrome_options'])
@@ -12,3 +13,4 @@ def setup_browser():
 @after.all
 def teardown_browser(total):
     world.browser.quit()
+    os.system('mb stop')
