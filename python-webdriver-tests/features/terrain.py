00e15db0911a243cd88ce2d7bd9bc38e1efb8248
LiangZhen <phoebusliang@gmail.com>
2015-05-21 19:01:07 +0800

    Add lazy load webdriver test case with lettuce.

--- python-webdriver-tests/features/terrain.py
+++ python-webdriver-tests/features/terrain.py
@@ -2,15 +2,15 @@ from lettuce import before, after, world
 from selenium import webdriver
 import os
 
-@before.all
-def setup_browser():
+@before.each_scenario
+def setup_browser(scenario):
     os.system('mb restart &')
     driver_arguments = {'chrome_options': webdriver.ChromeOptions()}
     driver_arguments['chrome_options'].add_argument('--no-sandbox')
     world.browser = webdriver.Chrome(chrome_options=driver_arguments['chrome_options'])
 
 
-@after.all
-def teardown_browser(total):
+@after.each_scenario
+def teardown_browser(scenario):
     world.browser.quit()
     os.system('mb stop')
