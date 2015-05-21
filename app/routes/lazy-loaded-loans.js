ea6ac4da660360c258856a541a2d53c910a91c8d
Jeff Lage <jefflage@me.com>
2015-05-21 09:25:30 -0400

    fixed lint errors

--- app/routes/lazy-loaded-loans.js
+++ app/routes/lazy-loaded-loans.js
@@ -2,11 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  setupController: function(controller){
-    //controller.set('totalCount',this.get('params').totalCount);
-  },
-
-  beforeModel: function(transition){
-    //this.set('params', transition.queryParams);
-  }
 });
