7751ee77590af36c22bd1e8345b99c8b10f47c0c
LiangZhen <phoebusliang@gmail.com>
2015-05-28 17:11:24 +0800

    1. Add prepare data for sortable column. 2. Finish webdriver test for sort of partial loaded data.

--- python-webdriver-tests/features/terrain.py
+++ python-webdriver-tests/features/terrain.py
@@ -5,7 +5,7 @@ import time
 @before.each_scenario
 def setup_browser(scenario):
     os.system('mb restart &')
-    time.sleep(5)
+    time.sleep(1)
     driver_arguments = {'chrome_options': webdriver.ChromeOptions()}
     driver_arguments['chrome_options'].add_argument('--no-sandbox')
     world.browser = webdriver.Chrome(chrome_options=driver_arguments['chrome_options'])
