adc595363eeb88bb70d588e443e63432aae4dbda
Chaoyang <cyjia@thoughtworks.com>
2015-08-12 16:06:26 +0800

    remove duplication in sliders setting.

--- app/views/index.js
+++ app/views/index.js
@@ -4,14 +4,11 @@ import TablesMixin from '../mixins/features';
 export default Ember.View.extend(TablesMixin, {
   afterRenderEvent: function () {
 
-    //Main slider
-    //-----------------------------------------------
-
     //Revolution Slider
     if (Ember.$(".slider-banner-container").length > 0) {
 
       Ember.$(".tp-bannertimer").show();
-      Ember.$('.slider-banner-container .slider-banner').show().revolution({
+      var defaultOptions = Ember.Object.create({
         delay: 10000,
         startwidth: 1140,
         startheight: 520,
@@ -56,111 +53,38 @@ export default Ember.View.extend(TablesMixin, {
 
         hideSliderAtLimit: 0,
         hideCaptionAtLimit: 0,
-        hideAllCaptionAtLilmit: 0,
+        hideAllCaptionAtLimit: 0,
         startWithSlide: 0
       });
 
-      Ember.$('.slider-banner-container .slider-banner-2').show().revolution({
-        delay: 10000,
-        startwidth: 1140,
-        startheight: 520,
+      var sliders = [
+        {
+          selector: '.slider-banner',
+          options: {}
+        },
+        {
+          selector: '.slider-banner-2',
+          options: {
+            navigationStyle: "preview4"
+          }
+        },
+        {
+          selector: '.slider-banner-2',
+          options: {
+            navigationStyle: "preview5",
+            parallax: "mouse",
+            parallaxBgFreeze: "on",
+            parallaxLevels: [3, 2, 1]
+          }
+        }
+      ];
 
-        navigationArrows: "solo",
-
-        navigationStyle: "preview4",
-        navigationHAlign: "center",
-        navigationVAlign: "bottom",
-        navigationHOffset: 0,
-        navigationVOffset: 20,
-
-        soloArrowLeftHalign: "left",
-        soloArrowLeftValign: "center",
-        soloArrowLeftHOffset: 20,
-        soloArrowLeftVOffset: 0,
-
-        soloArrowRightHalign: "right",
-        soloArrowRightValign: "center",
-        soloArrowRightHOffset: 20,
-        soloArrowRightVOffset: 0,
-
-        fullWidth: "on",
-
-        spinner: "spinner0",
-
-        stopLoop: "off",
-        stopAfterLoops: -1,
-        stopAtSlide: -1,
-        onHoverStop: "off",
-
-        shuffle: "off",
-
-        autoHeight: "off",
-        forceFullWidth: "off",
-
-        hideThumbsOnMobile: "off",
-        hideNavDelayOnMobile: 1500,
-        hideBulletsOnMobile: "off",
-        hideArrowsOnMobile: "off",
-        hideThumbsUnderResolution: 0,
-
-        hideSliderAtLimit: 0,
-        hideCaptionAtLimit: 0,
-        hideAllCaptionAtLilmit: 0,
-        startWithSlide: 0
-      });
-
-      Ember.$('.slider-banner-container .slider-banner-3').show().revolution({
-        delay: 10000,
-        startwidth: 1140,
-        startheight: 520,
-        dottedOverlay: "twoxtwo",
-
-        parallax: "mouse",
-        parallaxBgFreeze: "on",
-        parallaxLevels: [3, 2, 1],
-
-        navigationArrows: "solo",
-
-        navigationStyle: "preview5",
-        navigationHAlign: "center",
-        navigationVAlign: "bottom",
-        navigationHOffset: 0,
-        navigationVOffset: 20,
-
-        soloArrowLeftHalign: "left",
-        soloArrowLeftValign: "center",
-        soloArrowLeftHOffset: 20,
-        soloArrowLeftVOffset: 0,
-
-        soloArrowRightHalign: "right",
-        soloArrowRightValign: "center",
-        soloArrowRightHOffset: 20,
-        soloArrowRightVOffset: 0,
-
-        fullWidth: "on",
-
-        spinner: "spinner0",
-
-        stopLoop: "off",
-        stopAfterLoops: -1,
-        stopAtSlide: -1,
-        onHoverStop: "off",
-
-        shuffle: "off",
-
-        autoHeight: "off",
-        forceFullWidth: "off",
-
-        hideThumbsOnMobile: "off",
-        hideNavDelayOnMobile: 1500,
-        hideBulletsOnMobile: "off",
-        hideArrowsOnMobile: "off",
-        hideThumbsUnderResolution: 0,
-
-        hideSliderAtLimit: 0,
-        hideCaptionAtLimit: 0,
-        hideAllCaptionAtLilmit: 0,
-        startWithSlide: 0
+      sliders.forEach(function(s) {
+        var options = Ember.Object.create(defaultOptions).setProperties(s.options);
+        Ember.$('.slider-banner-container')
+          .find(s.selector)
+          .show()
+          .revolution(options);
       });
     }
 
