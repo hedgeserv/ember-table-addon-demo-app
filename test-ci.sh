855d662da395e02b6cab99be114bf9a343875127
phoebusliang@gmail.com <phoebusliang@gmail.com>
2015-10-29 19:00:26 +0800

    Add --mock when start the mountebank in test-ci.sh file to fix the webdriver tests for demo app.

--- test-ci.sh
+++ test-ci.sh
@@ -6,7 +6,7 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo "${green}Start Mountebank ...${reset}"
-mb --allowCORS --allowInjection --loglevel error &
+mb --allowCORS --allowInjection --mock --loglevel error &
 sleep 1
 
 echo "${green}Run ember test ...${reset}"
